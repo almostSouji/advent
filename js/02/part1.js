4133f66331162a0db0b64c47f03e99fafa4ba1f8
almostSouji <timoqueezle@gmail.com>
2018-12-02 11:38:56 +0100

    solution day2 (js)

---
+++ js/02/part1.js
@@ -0,0 +1,24 @@
+const fs = require('fs');
+const input = fs.readFileSync('./input.txt', 'utf-8');
+const arr = input.split('\n');
+let two = 0;
+let three = 0;
+
+for (const element of arr) {
+	let hasTwo = false;
+	let hasThree = false;
+	const symbols = element.split('');
+	for (const symbol of symbols) {
+		const f = symbols.filter(e => e === symbol);
+		if (f.length === 2) {
+			hasTwo = true;
+		}
+		if (f.length === 3) {
+			hasThree = true;
+		}
+	}
+	if (hasTwo) two++;
+	if (hasThree) three++;
+}
+
+console.log(`Checksum: ${two * three}`);
