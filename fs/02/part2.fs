2381ee9a73fee6c79b78d734d321187283a4187d
almostSouji <timoqueezle@gmail.com>
2018-12-03 10:03:04 +0100

    solve F# P2

--- fs/02/part2.fs
+++ fs/02/part2.fs
@@ -1,2 +1,22 @@
 module PartTwo
 
+let input = System.IO.File.ReadLines("./input.txt")
+            |> Seq.toList
+            |> List.map Seq.toList
+
+let compare (l1 : 'a list) (l2 : 'a list) =
+    let (common, unique) = 
+        List.zip l1 l2 
+        |> List.partition (fun (a,b) -> a = b)
+
+    if List.length unique = 1 then List.map (fun (a,_) -> a) common else []
+
+let solve (lines : 'a list list) = 
+    lines
+    |> List.collect (fun line -> List.map (fun lin -> compare lin line)lines)
+    |> List.concat
+
+solve input
+|> List.toArray
+|> System.String
+|> printfn "The right ID is: %A"
