b3f81d721e877a8392a13994d37480b67933fe5c
almostSouji <timoqueezle@gmail.com>
2019-12-08 01:27:54 +0100

    solve: 07 part 2

--- 2019/07/computer.py
+++ 2019/07/computer.py
@@ -1,11 +1,41 @@
+""" Computes int codes with given parameters
+
+Args:
+	program (string[]): The program to run
+	parameters (string[]): Parmeters the program can read from
+	pointer (int): Starting point for the pointer, 0 if none
+
+About:
+	Instructions consist of OP-CODES and parameter modes as well as parameters
+	Legal parameters depend on instruction given
+	ABCDE
+	321OP
+	A,B,C give modes for param 3,2,1
+
+MODES:
+	0 POSITION (use value as position)
+	1 IMMEDIATE (use value as direct value)
+
+OP-CODES:
+	1 ADD (add p1 + p2, save in p3)
+	2 MULTIPLY (multiply p1 * p2, save in p3)
+	3 GET-INPUT (get input, save in p1)
+	4 RETURN (print p1)
+	5 JUMP-IF-TRUE (if p1 is non-zero set pointer to p2)
+	6 JUMP-IF-FALSE (if p1 is zero set pointer to p2)
+	7 LESS THAN (if p1 is less than p2 store 1 in p3, else store 0 in p3)
+	8 EQUALS (if p1 equals p2 store 1 in p3, else store 0 in p3)
+	99 HALT
+"""
+
 def compute(program, parameters, pointer = 0):
 	prog = program[:]
 	finished = False
 	maxParamCount = 3
 	while not finished:
-		instruction = prog[pointer]
+		instruction = prog[pointer].zfill(maxParamCount + 2)
 		op = int(instruction[-2:])
-		modes = str(instruction)[:-2].zfill(maxParamCount)#
+		modes = str(instruction)[:-2]
 		# 3 parameter instructions
 		if op in [1,2,7,8]:
 			p1 = int(prog[int(prog[pointer + 1])]) if modes[2] == "0" else int(prog[pointer + 1])
@@ -24,20 +54,20 @@ def compute(program, parameters, pointer = 0):
 			p1 = int(prog[int(prog[pointer + 1])]) if modes[2] == "0" else int(prog[pointer + 1])
 			p2 = int(prog[int(prog[pointer + 2])]) if modes[1] == "0" else int(prog[pointer + 2])
 			if op == 5:
-				pointer = p2 if p1 == 1 else pointer + 3
+				pointer = p2 if p1 != 0 else pointer + 3
 			else:
 				pointer = p2 if p1 == 0 else pointer + 3
 		# 1 parameter instructions
 		elif op in [3,4]:
 			if op == 3:
 				prog[int(prog[pointer + 1])] = parameters.pop()
+				pointer = pointer + 2
 			else:
 				val = int(prog[int(prog[pointer + 1])]) if modes [2] == "0" else int(prog[pointer + 1])
-				return (prog, val, pointer)
-			pointer = pointer + 2
+				pointer = pointer + 2
+				return (prog, pointer, False, val)
 		elif op == 99:
-			finished = True
+			return (prog, pointer, True, 0)
 		else:
 			raise Exception("Unknown opcode: {}".format(op))
-	print("------")
-	return True
+	raise Exception("The program did not finish with OP 99")
--- 2019/07/solution.py
+++ 2019/07/solution.py
@@ -14,4 +14,30 @@ def calcOut (config):
 	return val
 
 values = list(map(lambda config : calcOut(config),permutations))
-print("Part 1: Max thruster output: {} with thruster signal {}".format(max(values), permutations[values.index(max(values))]))
+print("Part 1: Max thruster output: {} with thruster signals: {}".format(max(values), permutations[values.index(max(values))]))
+
+p2permutations = list(map(list, itert.permutations([5, 6, 7, 8, 9])))
+
+def p2calcOut (config):
+	# (state, pointer, finished, last_value)
+	thrusterstates = []
+	val = 0
+	
+	for i in range(0, len(config)):
+		thrusterstates.append(compute(program, [val, config[i]]))
+		val = thrusterstates[-1][-1]
+	while any(list(map(lambda state: not state[2] ,thrusterstates))):
+		for i in range (0, len(config)):
+			c_state, c_pointer, c_finished, c_last_value = thrusterstates[i]
+			state, pointer, is_finished, value = compute(c_state, [val], c_pointer)
+			if (not is_finished):
+				thrusterstates[i] = (state, pointer, is_finished, value)
+				val = value
+			else:
+				thrusterstates[i] = (state, pointer, is_finished, c_last_value)
+	return (thrusterstates[-1][3], config)
+
+values = list(map(lambda config : p2calcOut(config), p2permutations))
+
+ceil = max(values, key=lambda x: x[0])
+print("Part 2: Max thruster output: {} with thruster signals: {}".format(ceil[0], ceil[1]))
