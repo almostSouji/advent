f3947bbb535e5419d55529403245bfb0891042ae
almostSouji <timoqueezle@gmail.com>
2019-12-08 15:48:51 +0100

    solve: 08 part 1+2

---
+++ 2019/08/solution.py
@@ -0,0 +1,40 @@
+import math
+
+with open("input.txt", "r") as file:
+	data = file.read()
+
+def chunk_list(lst, n):
+	for i in range(0, len(lst), n):
+		yield lst[i:i + n]
+
+targetchunk = []
+targetchunkzeros = math.inf
+chunks = list(chunk_list(data, 25 * 6))
+
+for chunk in chunks:
+	zeros = len([letter for letter in chunk if letter == "0"])
+	if zeros < targetchunkzeros:
+		targetchunk = chunk
+		targetchunkzeros = zeros
+
+ones = len([letter for letter in targetchunk if letter == "1"])
+twos = len([letter for letter in targetchunk if letter == "2"])
+
+print("Part 1: Answer: {} * {} = {}".format(ones, twos, ones * twos))
+
+colormap = {
+	"0": " ", # black
+	"1": "0", # white
+	"2": " "  # transparent
+}
+
+def compress (pixel, layerlist):
+	for layer in layerlist:
+		if layer[pixel] != "2":
+			return layer[pixel]
+
+picture = [colormap[compress(pixel, chunks)] for pixel in range(25 * 6)]
+
+print("Part 2: Password:\n")
+for chunk in (chunk_list("".join(picture), 25)):
+	print(chunk)
