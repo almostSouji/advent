e9189df4c4e47d39426ea91684d08696903dc792
almostSouji <timoqueezle@gmail.com>
2020-12-01 13:47:10 +0100

    01:solve

---
+++ 2020/01_expense-report/solution.py
@@ -0,0 +1,19 @@
+with open("input.txt", "r") as file:
+    ns = list(map(lambda x: int(x), file.readlines()))
+
+def solve1(ns):
+    for n in ns:
+        for m in ns:
+            if m + n == 2020:
+                return m * n
+
+
+def solve2(ns):
+    for n in ns:
+        for m in ns:
+            for o in ns:
+                if m + n + o == 2020:
+                    return m * n * o
+
+print(f"Part 1: {solve1(ns)}")
+print(f"Part 2: {solve2(ns)}")
