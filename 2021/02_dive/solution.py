ec4b79adf339e37880eff1052fb8708991b2b006
almostSouji <timoqueezle@gmail.com>
2021-12-02 11:08:16 +0100

    02: solve

---
+++ 2021/02_dive/solution.py
@@ -0,0 +1,65 @@
+import time
+
+begin = time.time()
+
+###
+
+with open("./input.txt", "r") as file:
+    ns = file.readlines()
+
+
+# ~ req. py 3.10
+# def solve1(ns):
+#     h = 0
+#     d = 0
+#     for n in ns:
+#         (op, val_s) = n.split()
+#         val = int(val_s)
+#         match op:
+#             case "forward":
+#                 h += val
+#             case "down":
+#                 d += val
+#             case "up":
+#                 d -= val
+#     return h * d
+
+def solve1(ns):
+    h = 0
+    d = 0
+    for n in ns:
+        (op, val_s) = n.split()
+        val = int(val_s)
+        if (op == "forward"):
+            h += val
+        elif (op == "down"):
+            d += val
+        elif (op == "up"):
+            d -= val
+    return h * d
+
+
+def solve2(ns):
+    h = 0
+    d = 0
+    a = 0
+    for n in ns:
+        (op, val_s) = n.split()
+        val = int(val_s)
+        if (op == "forward"):
+            h += val
+            d += (val * a)
+        elif (op == "down"):
+            a += val
+        elif (op == "up"):
+            a -= val
+    return h * d
+
+
+print(f"Part 1: {solve1(ns)}")
+print(f"Part 2: {solve2(ns)}")
+
+###
+
+end = time.time()
+print(f"Runtime: {(end - begin) * 1000}ms")
