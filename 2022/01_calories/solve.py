219fe2042b83906438d6a3dfcca9bb6bd501669f
almostSouji <timoqueezle@gmail.com>
2022-12-01 11:07:23 +0100

    solve(d1): p1,2

--- 2022/01_calories/solve.py
+++ 2022/01_calories/solve.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 import sys
 import pprint
-
+import time
 
 def debug(*args, pretty=False, **kwargs):
     "print() to stderr for debuggin purposes"
@@ -14,9 +14,26 @@ def debug(*args, pretty=False, **kwargs):
             file=sys.stderr,
         )
 
+begin = time.time()
 
+####
+
+elves = []
+current_elve = []
 for raw_line in sys.stdin:
     line = raw_line.strip()
     if len(line) == 0:
-        break
-    print(line)
+        elves.append(sum(current_elve))
+        current_elve = []
+        continue
+    current_elve.append(int(line))
+
+elves.append(sum(current_elve))
+
+print(max(elves))
+print(sum(sorted(elves)[-3:]))
+
+###
+
+end = time.time()
+debug(f"Runtime: {(end - begin) * 1000}ms")
