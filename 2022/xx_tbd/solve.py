781fdbca7a261abfa0a7d2d8e2690de6ab537696
almostSouji <timoqueezle@gmail.com>
2022-12-03 13:48:46 +0100

    chore(cli): remove timer
    * that's what time cli is for

--- 2022/xx_tbd/solve.py
+++ 2022/xx_tbd/solve.py
@@ -1,7 +1,6 @@
 #!/usr/bin/env python3
 import sys
 import pprint
-import time
 
 def debug(*args, pretty=False, **kwargs):
     "print() to stderr for debuggin purposes"
@@ -14,17 +13,9 @@ def debug(*args, pretty=False, **kwargs):
             file=sys.stderr,
         )
 
-begin = time.time()
-
 ####
 
 for raw_line in sys.stdin:
     line = raw_line.strip()
     if len(line) == 0:
         continue
-
-
-###
-
-end = time.time()
-debug(f"Runtime: {(end - begin) * 1000}ms")
