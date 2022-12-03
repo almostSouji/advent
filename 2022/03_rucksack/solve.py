567480ef1a727f0520951128715863ed3f248619
almostSouji <timoqueezle@gmail.com>
2022-12-03 13:22:09 +0100

    solve(d3): 1, 2

---
+++ 2022/03_rucksack/solve.py
@@ -0,0 +1,43 @@
+#!/usr/bin/env python3
+import sys
+import pprint
+from functools import reduce
+
+def debug(*args, pretty=False, **kwargs):
+    "print() to stderr for debuggin purposes"
+    if pretty:
+        pprint.pprint(*args, **kwargs, stream=sys.stderr)
+    else:
+        print(
+            *args,
+            **kwargs,
+            file=sys.stderr,
+        )
+
+def score(c: chr):
+    o = ord(c)
+    isUpper = o <= ord("Z")
+    offset = ord("A") - 27 if isUpper else ord("a") - 1
+    return o - offset
+
+scores = []
+group_scores = []
+curr_group = []
+for raw_line in sys.stdin:
+    line = raw_line.strip()
+    if len(line) == 0:
+        continue
+
+    curr_group.append(set(line))
+    if len(curr_group) == 3:
+        common_item = reduce(lambda a, b: a.intersection(b), curr_group).pop()
+        group_scores.append(score(common_item))
+        curr_group = []
+
+    breakpoint = int(len(line) / 2)
+    [fst, snd] = [set(line[:breakpoint]), set(line[breakpoint:])]
+    dupe = fst.intersection(snd).pop()
+    scores.append(score(dupe))
+
+print(sum(scores))
+print(sum(group_scores))
