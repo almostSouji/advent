37000cd291c388cdbd330282dfff6765f2f2dbc2
almostSouji <timoqueezle@gmail.com>
2022-12-18 01:13:19 +0100

    solve(d17): 2

--- 2022/17_pyroclastic_flow/solve.py
+++ 2022/17_pyroclastic_flow/solve.py
@@ -16,98 +16,103 @@ def debug(*args, pretty=False, **kwargs):
 
 
 def debug_plot_set(s):
+    h = max(map(lambda x: x.imag, s))
     a = ["".join(["#" if c+r*1j in s else "." for c in range(7)])
-         for r in range(int(get_h()+5))]
+         for r in range(int(h+3))]
     a.reverse()
     for row in a:
-        debug(row)
-
+        print(row)
 
 ####
 
-def s1(h):
-    return [2+h*1j, 3+h*1j, 4+h*1j, 5+h*1j]
+
+SHAPES = [
+    [0, 1, 2, 3],
+    [0+1j, 1+1j, 2+1j, 1, 1+2j],
+    [0, 1, 2, 2+1j, 2+2j],
+    [0, 1j, 2j, 3j],
+    [0, 1, 1j, 1+1j]
+]
 
 
-def s2(h):
-    return [3+2j+h*1j, 2+1j+h*1j, 3+1j+h*1j, 4+1j+h*1j, 3+h*1j]
+def is_collision(a, rocks):
+    for c in a:
+        if c.real not in range(0, 7):
+            return True
+        if c.imag < 0:
+            return True
 
-
-def s3(h):
-    return [4+2j+h*1j, 4+1j+h*1j, 2+h*1j, 3+h*1j, 4+h*1j]
-
-
-def s4(h):
-    return [2+3j+h*1j, 2+2j+h*1j, 2+1j+h*1j, 2+h*1j]
-
-
-def s5(h):
-    return [2+1j+h*1j, 3+1j+h*1j, 2+h*1j, 3+h*1j]
-
-
-rocks = set()
-
-
-def is_collision(a):
     return bool(set(a) & rocks)
 
 
-def r(rock):
-    n = [x+1 for x in rock]
-    if is_collision(n):
-        return rock
-    if sorted(map(lambda x: x.real, n))[-1] > 6:
-        return rock
-    return n
+def skyline(s):
+    res = [-1] * 7
+
+    for x in s:
+        r = int(x.real)
+        i = int(x.imag)
+
+        res[r] = max(res[r], i)
+
+    f = max(res)
+    return tuple(x - f for x in res)
 
 
-def l(rock):
-    n = [x-1 for x in rock]
-    if is_collision(n):
-        return rock
-    if sorted(map(lambda x: x.real, n))[0] < 0:
-        return rock
-    return n
+jets = [1 if x == ">" else -1 for x in input()]
+
+T = 1000000000000
 
 
-def d(rock):
-    n = [x-1j for x in rock]
-    if is_collision(n):
-        return False
-    if sorted(map(lambda x: x.imag, n))[0] < 0:
-        return False
-    return n
+def solve(n):
+    rocks = set()
+    states = {}
+
+    ji = 0
+    si = 0
+    i = 0
+    h = -1
+
+    while i <= n:
+        rock = [x+2+(h+4)*1j for x in SHAPES[si]]
+        while True:
+            jo = jets[ji]
+            nex = [x+jo for x in rock]
+            if not is_collision(nex, rocks):
+                rock = nex
+
+            ji = (ji + 1) % len(jets)
+
+            nex = [x-1j for x in rock]
+            if not is_collision(nex, rocks):
+                rock = nex
+            else:
+                break
+
+        i += 1
+        rocks.update(rock)
+        h = max(x.imag for x in rocks)
+        si = (si+1) % len(SHAPES)
+
+        sky = skyline(rocks)
+        key = (si, ji, sky)
+
+        if key in states:
+            h_prev, i_prev = states[key]
+            h_delta = h - h_prev
+            i_delta = i+1 - i_prev
+
+            todo = n - i+1
+            repetitions = todo // i_delta
+
+            h_skipped = h_delta * repetitions + 1
+            i += i_delta * repetitions
+
+            states = {}
+        else:
+            states[key] = (h, i+1)
+
+    return(int(h-1 + h_skipped))
 
 
-def get_h():
-    if not rocks:
-        return -1
-    return max(map(lambda x: x.imag, rocks))
-
-
-c = open(0).read().strip()
-types = [s1, s2, s3, s4, s5]
-
-p = 0
-
-for i in range(0, 2023):
-
-    rock = types[i % 5](get_h()+4)
-    while True:
-        match(c[p % len(c)]):
-            case ">":
-                rock = r(rock)
-            case "<":
-                rock = l(rock)
-        p += 1
-
-        rock_before = [x for x in rock]
-        rock = d(rock)
-        if not rock:
-            rock = rock_before
-            break
-
-    rocks.update(rock)
-
-
-print(get_h()-1)
+print(f"p1: {solve(2022)} -1, off by one :(")
+print(f"p2: {solve(T)}")
