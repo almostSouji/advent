5feeb6e5f492cfcdc057d4ffcfc83133dd9a551c
almostSouji <timoqueezle@gmail.com>
2023-12-02 13:35:22 +0100

    solve(d2): 2

--- 2023/02_cube/solve.py
+++ 2023/02_cube/solve.py
@@ -23,21 +23,35 @@ total_cubes = {
 }
 
 s = 0
+s2 = 0
 for line in open(0):
     game, rounds = line.split(":")
     _, id = game.split(" ")
     id = int(id)
+    minimums = {
+        "red": 0,
+        "green": 0,
+        "blue": 0
+    }
+
+    possible = True
     for run in rounds.split(";"):
         for cube in run.split(","):
             num, color = cube.strip().split(" ")
             num = int(num)
             available = total_cubes.get(color, 0)
+
+            if minimums.get(color, 0) < num:
+                minimums[color] = num
+
             if available < num:
-                break
-        else:
-            continue
-        break
-    else:
+                possible = False
+    if possible:
         s += id
+    power = 1
+    for val in minimums.values():
+        power *= val
+    s2 += power
 
 print(s)
+print(s2)
