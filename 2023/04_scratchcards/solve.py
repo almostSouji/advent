6bfad45e2c2ec0aae4bdd70c3e59d02de133dc95
almostSouji <timoqueezle@gmail.com>
2023-12-04 12:08:08 +0100

    solve(d4): 2

--- 2023/04_scratchcards/solve.py
+++ 2023/04_scratchcards/solve.py
@@ -18,6 +18,7 @@ def debug(*args, pretty=False, **kwargs):
 ####
 
 cards = dict()
+todo = []
 
 s = 0
 for i, line in enumerate(open(0)):
@@ -26,8 +27,25 @@ for i, line in enumerate(open(0)):
     wns = [int(x) for x in _wnstr.split()[2::]]
     ns = [int(x) for x in nstr.split()]
     matches = [x for x in ns if x in wns]
-    s += 2 ** (len(matches) - 1) if len(matches) else 0
+
+    nm = len(matches)
+    score = 2 ** (nm - 1) if nm > 0 else 0
+    s += score
+
+    c = (i, nm, score)
+    cards[i] = c
+    todo.append(c)
 
 print(s)
 
+for c in todo:
+    i, nm, score = c
+
+    for j in range(i + 1, i + nm + 1):
+        todo.append(cards[j])
+
+s2 = len(todo)
+print(s2)
+
 assert s == 19855, s
+assert s2 == 10378710, s2
