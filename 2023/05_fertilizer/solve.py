f3bf030bb12164b0716d578aae58e11b497e42e1
almostSouji <timoqueezle@gmail.com>
2023-12-06 00:05:01 +0100

    solve(d5): 2

--- 2023/05_fertilizer/solve.py
+++ 2023/05_fertilizer/solve.py
@@ -18,29 +18,63 @@ def debug(*args, pretty=False, **kwargs):
 ####
 
 seeds = None
+seeds2 = []
 for block in open(0).read().split("\n\n"):
     if not seeds:
         seeds = [int(x) for x in block.split()[1::]]
+
+        for i in range(0, len(seeds), 2):
+            seeds2.append((seeds[i], seeds[i] + seeds[i + 1]))
+
         continue
 
     rs = []
     for line in block.split("\n")[1::]:
-        dest_start, src_start, range_len = [int(x) for x in line.split()]
-        rs.append((dest_start, src_start, range_len))
+        dest_s, src_s, range_l = [int(x) for x in line.split()]
+        rs.append((dest_s, src_s, range_l))
 
-    converted = []
+    conv = []
     for seed in seeds:
         for r in rs:
-            dest_start, src_start, range_len = r
+            dest_s, src_s, range_l = r
 
-            if src_start <= seed < src_start + range_len:
-                converted.append(dest_start + (seed - src_start))
+            if src_s <= seed < src_s + range_l:
+                delta = dest_s - src_s
+                conv.append(seed + delta)
                 break
         else:
-            converted.append(seed)
-    seeds = converted
+            conv.append(seed)
+    seeds = conv
 
-s = min(seeds)
-print(s)
+    conv = []
+    for seed in seeds2:
+        start, end = seed
 
-assert s == 510109797, s
+        for r in rs:
+            dest_s, src_s, range_l = r
+            overlap_s = max(start, src_s)
+            overlap_e = min(end, src_s + range_l)
+
+            if overlap_s < overlap_e:
+                delta = dest_s - src_s
+
+                conv.append((overlap_s + delta, overlap_e + delta))
+
+                if start < overlap_s:
+                    seeds2.append((start, overlap_s))
+                if end > overlap_e:
+                    seeds2.append((end, overlap_e))
+
+                break
+        else:
+            conv.append(seed)
+    seeds2 = conv
+
+
+start = min(seeds)
+print(start)
+s2 = min(seeds2)[0]
+print(s2)
+
+assert start == 510109797, start
+assert s2 == 9622622, s2
