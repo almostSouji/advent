3305a5c115040eb7d0dcf2c405c6b87e81530337
almostSouji <timoqueezle@gmail.com>
2023-12-07 16:12:27 +0100

    solve(d7): 2

--- 2023/07_camel_cards/solve.py
+++ 2023/07_camel_cards/solve.py
@@ -19,49 +19,57 @@ def debug(*args, pretty=False, **kwargs):
 ####
 
 alphabet = list("AKQJT98765432")
+alphabet2 = list("AKQT98765432J")
 alphabet.reverse()
+alphabet2.reverse()
 
-hands = []
-for line in open(0):
-    cards, bid = line.strip().split()
-    bid = int(bid)
 
-    score = 0
+def score_hand(cards, joker=False):
     counter = {}
     for card in cards:
-        current = counter.get(card, 0)
-        counter[card] = current + 1
+        counter[card] = counter.get(card, 0) + 1
+    jokers = counter.get("J", 0)
 
-    vals = list(counter.values())
+    vals = (
+        [x[1] for x in counter.items() if x[0] != "J"]
+        if joker
+        else list(counter.values())
+    )
     vals.sort(reverse=True)
 
-    if vals[0] == 5:  # Five of a kind
-        score = 7
+    if joker and len(vals):
+        vals[0] += jokers
+
+    if joker and jokers == 5:  # Five of a kind (all jokers)
+        return 7
+
+    elif vals[0] == 5:  # Five of a kind
+        return 7
+
     elif vals[0] == 4:  # Four of a kind
-        score = 6
+        return 6
 
     elif vals[0] == 3 and vals[1] == 2:  # Full house
-        score = 5
+        return 5
 
     elif vals[0] == 3:  # Three of a kind
-        score = 4
+        return 4
 
     elif vals[0] == 2 and vals[1] == 2:  # Two pair
-        score = 3
+        return 3
 
     elif vals[0] == 2:  # One pair
-        score = 2
+        return 2
+
     else:  # High card
-        score = 1
-
-    hands.append((cards, score, bid))
+        return 1
 
 
-def card_strength(card):
-    return alphabet.index(card)
+def card_strength(card, joker=False):
+    return alphabet2.index(card) if joker else alphabet.index(card)
 
 
-def sort(a, b):
+def sort(a, b, joker=False):
     a_cards, a_score, _ = a
     b_cards, b_score, _ = b
 
@@ -71,8 +79,8 @@ def sort(a, b):
         return -1
     else:
         for i, c in enumerate(a_cards):
-            a_s = card_strength(c)
-            b_s = card_strength(b_cards[i])
+            a_s = card_strength(c, joker)
+            b_s = card_strength(b_cards[i], joker)
 
             if a_s > b_s:
                 return 1
@@ -81,14 +89,33 @@ def sort(a, b):
         return 0
 
 
+def evaluate_hands(hands):
+    res = 0
+    for i, (_, _, bid) in enumerate(hands):
+        rank = len(hands) - i
+        res += rank * bid
+    return res
+
+
+hands = []
+hands2 = []
+for line in open(0):
+    cards, bid = line.strip().split()
+    bid = int(bid)
+
+    hands.append((cards, score_hand(cards), bid))
+    hands2.append((cards, score_hand(cards, joker=True), bid))
+
+
 hands.sort(key=cmp_to_key(sort), reverse=True)
+hands2.sort(key=cmp_to_key(lambda a, b: sort(a, b, joker=True)), reverse=True)
 
-s = 0
 
-for i, (cards, score, bid) in enumerate(hands):
-    rank = len(hands) - i
-    s += rank * bid
-
+s = evaluate_hands(hands)
 print(s)
 
+s2 = evaluate_hands(hands2)
+print(s2)
+
 assert s == 250254244, s
+assert s2 == 250087440, s2
