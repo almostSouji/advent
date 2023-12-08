7b5d3a8c520435d11f0caca8891eddfe1d25c454
almostSouji <timoqueezle@gmail.com>
2023-12-08 16:11:14 +0100

    solve(d8): 2

--- 2023/08_haunted_wasteland/solve.py
+++ 2023/08_haunted_wasteland/solve.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 import sys
 import pprint
+from math import lcm
 
 
 def debug(*args, pretty=False, **kwargs):
@@ -33,7 +34,6 @@ for line in open(0):
 
     m[nodename] = (nodename, a, b)
 
-
 s = 0
 node = m["AAA"]
 while True:
@@ -50,5 +50,30 @@ while True:
 
     s += 1
 
+nodes = [x for x in m.values() if x[0].endswith("A")]
+seen = [0 for _ in range(len(nodes))]
+
+c = 0
+while nodes:
+    i = c % len(inst)
+
+    if 0 not in seen:
+        break
+
+    for ni, node in enumerate(nodes):
+        name, l, r = node
+        if name.endswith("Z"):
+            seen[ni] = c
+        if inst[i] == "L":
+            nodes[ni] = m[l]
+        else:
+            nodes[ni] = m[r]
+    c += 1
+
+s2 = lcm(*seen)
+
+
 print(s)
+print(s2)
 assert s == 14257, s
+assert s2 == 16187743689077, s2
