bfe48adcbcbe228e404d7546f20fc4994a027379
almostSouji <timoqueezle@gmail.com>
2023-12-09 13:28:00 +0100

    solve(d9): 2

--- 2023/09_mirage_maintenance/solve.py
+++ 2023/09_mirage_maintenance/solve.py
@@ -18,6 +18,7 @@ def debug(*args, pretty=False, **kwargs):
 ####
 
 s = 0
+s2 = 0
 for line in open(0).readlines():
     line = [int(x) for x in line.split()]
     b = []
@@ -27,8 +28,15 @@ for line in open(0).readlines():
     b.append(line)
     b.reverse()
     pred = 0
+    past = 0
     for e in b:
+        past = e[0] - past
         pred += e[-1]
     s += pred
+    s2 += past
 
 print(s)
+print(s2)
+
+assert s == 2005352194, s
+assert s2 == 1077, s2
