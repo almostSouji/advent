11ed401c03590631d857e0f8063d6e047c4b5e94
almostSouji <timoqueezle@gmail.com>
2023-12-10 22:07:37 +0100

    solve(d10): 2

--- 2023/10_pipe_maze/solve.py
+++ 2023/10_pipe_maze/solve.py
@@ -18,12 +18,12 @@ def debug(*args, pretty=False, **kwargs):
 ####
 
 m = {}
-s = None
+start = None
 for x, line in enumerate(open(0)):
     for y, c in enumerate(line.strip()):
         m[x + 1j * y] = c
         if c == "S":
-            s = x + 1j * y
+            start = x + 1j * y
 
 n = {
     (1, "|"): 1,
@@ -41,15 +41,20 @@ n = {
 }
 
 deltas = [1, -1, 1j, -1j]
-curr = [(s + d, d, [s]) for d in deltas if (d, m.get(s + d, None)) in n]
+curr = [
+    (start + d, d, [start, start + d])
+    for d in deltas
+    if (d, m.get(start + d, None)) in n
+]
 
-tick = 0
+lace = None
 while True:
     for i, (c, d, path) in enumerate(curr):
         delta = n.get((d, m[c]), None)
         next = c + delta
         for j, (_, _, p) in enumerate(curr):
             if next in p and i != j:
+                lace = [*curr[i][2], *reversed(curr[j][2][1:])]
                 break
         else:
             curr[i] = (next, delta, path + [next])
@@ -59,7 +64,58 @@ while True:
         continue
     break
 
-s = len(curr[j][2])
-print(s)
+s = int(len(lace) / 2)
 
+# find the real symbol of S
+assert lace[0] == start
+
+candidates = set("|-LJ7F")
+for elem in [lace[-1] - start, lace[1] - start]:
+    possible = set()
+    for k, v in n.items():
+        if v == elem:
+            possible.add(k[1])
+    candidates &= possible
+
+start_shape = candidates.pop()
+m[start] = start_shape
+
+# clean up the maze to not have to deal with weird pipes
+for a in range(x + 1):
+    for b in range(y + 1):
+        coord = a + 1j * b
+        if coord not in lace:
+            m[coord] = "."
+
+# find the tiles outside the maze
+outcoords = set()
+for a in range(x + 1):
+    inside = False
+    seenL = None
+    for b in range(y + 1):
+        assert a + 1j * b in m
+        c = m[a + 1j * b]
+
+        if c == "|":
+            assert seenL is None
+            inside = not inside
+        elif c in "LF":
+            assert seenL is None, (a, b, c, seenL)
+            seenL = c == "L"
+        elif c in "J7":
+            assert seenL is not None
+            if (seenL and c == "7") or (not seenL and c == "J"):
+                inside = not inside
+            seenL = None
+
+        coord = a + 1j * b
+        if not inside:
+            outcoords.add(coord)
+
+# remove the outside tiles and the lace tiles from all tiles to find the surrounded tiles
+s2 = len(set(m.keys()) - outcoords - set(lace))
+
+print(s)
+print(s2)
 assert s == 7066, s
+assert s2 == 401, s2
