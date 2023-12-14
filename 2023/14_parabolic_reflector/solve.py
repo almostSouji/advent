fb89aa19067c39dc835143347e788da7a55e3429
almostSouji <timoqueezle@gmail.com>
2023-12-14 13:43:39 +0100

    wip

--- 2023/14_parabolic_reflector/solve.py
+++ 2023/14_parabolic_reflector/solve.py
@@ -1,7 +1,15 @@
 #!/usr/bin/env python3
+from icecream import ic
 
-grid = open(0).read().split("\n")
-transposed = list(list(line) for line in zip(*grid))
+grid = [list(x) for x in open(0).read().splitlines()]
+
+
+def h(grid):
+    return tuple([tuple(line) for line in grid])
+
+
+def sort_grid(grid):
+    [sort(line) for line in grid]
 
 
 def sort(l):
@@ -15,11 +23,43 @@ def sort(l):
                 changed = True
 
 
-s = 0
-for line in transposed:
-    sort(line)
-    s += sum([len(line) - i for i, c in enumerate(line) if c == "O"])
+def transpose(grid):
+    return list(list(line) for line in zip(*grid))
 
-print(s)
 
+def turn(grid):
+    for i in range(4):
+        grid = transpose(grid)
+        sort_grid(grid)
+        grid = [x[::-1] for x in grid]
+
+    return grid
+
+
+def score(grid):
+    return sum(
+        [sum([len(line) - i for i, c in enumerate(line) if c == "O"]) for line in grid]
+    )
+
+
+c = transpose([line.copy() for line in grid])
+sort_grid(c)
+s = score(c)
+
+m = dict()
+
+for i in range(10):
+    grid = turn(grid)
+    hash = h(grid)
+    if hash in m:
+        ic(i)
+        break
+    else:
+        m[hash] = i
+    ic(grid)
+
+ic(h(grid))
+
+assert s == 136, s
+exit(0)
 assert s == 106990, s
