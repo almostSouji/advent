3461a66c5db2f6713dee377d0d52bd28209bf0f9
almostSouji <timoqueezle@gmail.com>
2025-12-01 17:24:10 +0100

    solve(d1): 2

--- 2025/01_secret_entrance/solve.py
+++ 2025/01_secret_entrance/solve.py
@@ -1,8 +1,8 @@
 #!/user/bine/env python3
 
-
 dial = 50
 count = 0
+clicks = 0
 for line in open(0):
     line = line.strip()
     op = line[0]
@@ -10,9 +10,16 @@ for line in open(0):
 
     assert op in ["R", "L"]
     next = dial + distance if op == "R" else dial - distance
+    offset = 1 if op == "R" else -1
+
+    zeros = [x for x in range(dial + offset, next + offset, offset) if x % 100 == 0]
+    clicks += len(zeros)
+
     dial = next % 100
 
     if dial == 0:
         count += 1
 
+
 print(f"part1: {count}")
+print(f"part2: {clicks}")
