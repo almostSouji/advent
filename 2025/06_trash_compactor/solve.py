1731b2258447750f4fb75629794b0b29b555dcbd
almostSouji <timoqueezle@gmail.com>
2025-12-07 01:24:16 +0100

    solve(d6): 2

--- 2025/06_trash_compactor/solve.py
+++ 2025/06_trash_compactor/solve.py
@@ -1,11 +1,12 @@
 from functools import reduce
 from operator import mul
 
-from icecream import ic
-
 grid = []
+p2_grid = []
 with open(0) as f:
     for line in f.readlines():
+        line = line.strip("\n")
+        p2_grid.append(line)
         grid.append(line.split())
 
 problems = []
@@ -27,4 +28,32 @@ for problem in problems:
     if op == "*":
         p1 += reduce(mul, numbers)
 
+
+max_p2_len = max([len(line) for line in p2_grid])
+p2_grid = [line.ljust(max_p2_len, " ") for line in p2_grid]
+
+
+p2_transposed = reversed(list(zip(*p2_grid)))
+
+p2 = 0
+numbers = []
+for line in p2_transposed:
+    joined = "".join(line[:-1]).strip()
+    if len(joined) <= 0:
+        numbers = []
+        continue
+
+    val = int(joined)
+    numbers.append(val)
+
+    op = line[-1]
+    if op == "+":
+        p2 += sum(numbers)
+        continue
+
+    if op == "*":
+        p2 += reduce(mul, numbers)
+
+
 print(f"part1: {p1}")
+print(f"part2: {p2}")
